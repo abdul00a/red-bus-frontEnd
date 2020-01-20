7bb2d07efc256ca46a898473d6eacc0eae3ed00c
Rishav Saini <rishav.saini@mountblue.tech>
2020-01-20 10:44:23 +0530

    Integrated the new UI with 'No buses found' alert

--- src/components/Trips/tripPlanner/busInfo/emptyBus/emptybus.jsx
+++ src/components/Trips/tripPlanner/busInfo/emptyBus/emptybus.jsx
@@ -4,10 +4,10 @@ import './emptybus.css';
 const EmptyBus = () => {
   return (
     <div>
-      <div className="no-bus">
-        <div className="sorry no">No</div>
-        <div className="sorry bus">BUS</div>
-        <div className="sorry avail">Available</div>
+      <div className='no-bus'>
+        <div className='sorry no'>No</div>
+        <div className='sorry bus'>BUS</div>
+        <div className='sorry avail'>Available</div>
       </div>
     </div>
   );
