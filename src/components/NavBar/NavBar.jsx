5eec63d0a4455a4a100eb94be2052c59f110306d
Abdul <abdul.ashraf@mountblue.tech>
2020-01-23 13:35:30 +0530

    Change UserID

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -28,38 +28,44 @@ class NavBar extends React.Component {
             <Menu
               onClick={this.handleClick}
               selectedKeys={[this.state.current]}
-              mode='horizontal'
+              mode="horizontal"
             >
-              <Menu.Item key='logo' disabled>
-                <div className='logo' />
+              <Menu.Item key="logo" disabled>
+                <div className="logo" />
               </Menu.Item>
-              <Menu.Item key='busTkt'><Link to="/">BUS TICKETS</Link></Menu.Item>
-              <Menu.Item key='pool'>
+              <Menu.Item key="busTkt">
+                <Link to="/">BUS TICKETS</Link>
+              </Menu.Item>
+              <Menu.Item key="pool">
                 rPool<sup>NEW</sup>
               </Menu.Item>
-              <Menu.Item key='hire'>BUS HIRE</Menu.Item>
-              <Menu.Item key='pilgrim'>PILGRIMAGES</Menu.Item>
+              <Menu.Item key="hire">BUS HIRE</Menu.Item>
+              <Menu.Item key="pilgrim">PILGRIMAGES</Menu.Item>
             </Menu>
           </Col>
           <Col span={4}>
-            {this.props.loggedIn ? (
+            {!this.props.loggedIn ? (
               <Menu
                 onClick={this.handleClick}
                 selectedKeys={[this.state.current]}
-                mode='horizontal'
+                mode="horizontal"
               >
                 <Menu.SubMenu
                   title={
-                    <span className='submenu-title-wrapper'>
-                      <div className='accounts-section'>
-                        <Icon style={{ fontSize: '25px' }} type='user' />
+                    <span className="submenu-title-wrapper">
+                      <div className="accounts-section">
+                        <Icon style={{ fontSize: '25px' }} type="user" />
                       </div>
                     </span>
                   }
                 >
                   <Menu.ItemGroup>
-                    <Menu.Item key='booking-history'><Link to='/bookinghistory'>My Tickets</Link></Menu.Item>
-                    <Menu.Item key='logout' onClick={this.props.logOut}>LogOut</Menu.Item>
+                    <Menu.Item key="booking-history">
+                      <Link to="/bookinghistory">My Tickets</Link>
+                    </Menu.Item>
+                    {/* <Menu.Item key="logout" onClick={this.props.logOut}>
+                      LogOut
+                    </Menu.Item> */}
                   </Menu.ItemGroup>
                 </Menu.SubMenu>
               </Menu>
@@ -67,13 +73,13 @@ class NavBar extends React.Component {
               <Menu
                 onClick={this.handleClick}
                 selectedKeys={[this.state.current]}
-                mode='horizontal'
+                mode="horizontal"
               >
                 <Menu.SubMenu
                   title={
-                    <span className='submenu-title-wrapper'>
-                      <div className='accounts-section'>
-                        <Icon style={{ fontSize: '25px' }} type='user' />
+                    <span className="submenu-title-wrapper">
+                      <div className="accounts-section">
+                        <Icon style={{ fontSize: '25px' }} type="user" />
                         Accounts
                       </div>
                     </span>
@@ -81,7 +87,7 @@ class NavBar extends React.Component {
                 >
                   <Menu.ItemGroup>
                     <Menu.Item
-                      key='login'
+                      key="login"
                       onClick={this.props.toggleDisplayLoginForm}
                     >
                       LogIn
@@ -93,19 +99,19 @@ class NavBar extends React.Component {
           </Col>
         </Row>
         <Modal
-          title='Login'
+          title="Login"
           visible={this.props.display}
           onOk={this.handleOk}
           onCancel={this.props.toggleDisplayLoginForm}
           footer={null}
-          width='700px'
+          width="700px"
         >
           <Row>
             <Col span={12}>
-              <div className='desktop-payment-offers' />
+              <div className="desktop-payment-offers" />
             </Col>
             <Col span={12}>
-              <div id='login-google'>
+              <div id="login-google">
                 <WrappedNormalLoginForm />
               </div>
             </Col>
@@ -128,7 +134,7 @@ const mapDispatchToProps = dispatch => {
   return {
     toggleDisplayLoginForm: () => dispatch(toggleDisplayLoginForm()),
     logIn: () => dispatch(logIn()),
-    logOut:()=>dispatch(logout()),
+    logOut: () => dispatch(logout())
   };
 };
 
